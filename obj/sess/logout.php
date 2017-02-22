968ad0f70ea8f79eb0f67a3af4b5046c4876c23d
handelik1 <handelik1@gmail.com>
2017-02-21 20:55:01 -0500

    Add files via upload

---
+++ obj/sess/logout.php
@@ -0,0 +1,84 @@
+<?php
+
+ /*************************************************************************************
+  This is the account logout routine. An established user has asked to leave the system.
+  We check these data against existing records and either setup the welcome page or
+  return an error message.
+  ************************************************************************************/
+
+ session_start();
+
+ require_once('./dbutils.php');									#  Contains DB helper functions
+
+ $send_request = preg_replace("/[^A-Za-z]/", '', $_POST['sendRequest']);
+
+ if(!isset($_SESSION['uname']) || !isset($_SESSION['pword']))	#  If there is no session copy of credentials
+   {															#  then we expect to find them in $_POST
+     $uname = preg_replace("~[^0-9A-Za-z\.\s\&\;\#\/\-\_\,\:\?\!]~", '', htmlentities($_POST['uname'], ENT_QUOTES));
+     if(strlen($uname) > 64)									#  Trim uname to permissible length
+       $uname = substr($uname, 0, 64);
+
+     $pword = preg_replace("~[^0-9A-Za-z\.\s\&\;\#\/\-\_\,\:\?\!]~", '', htmlentities($_POST['pword'], ENT_QUOTES));
+   }
+ else
+   {
+     $uname = preg_replace("~[^0-9A-Za-z\.\s\&\;\#\/\-\_\,\:\?\!]~", '', htmlentities($_SESSION['uname'], ENT_QUOTES));
+     if(strlen($uname) > 64)									#  Trim uname to permissible length
+       $uname = substr($uname, 0, 64);
+
+     $pword = preg_replace("~[^0-9A-Za-z\.\s\&\;\#\/\-\_\,\:\?\!]~", '', htmlentities($_SESSION['pword'], ENT_QUOTES));
+   }
+
+ if(strcmp($send_request, 'leightRLZRZ') == 0)					#  Run only if request came from JS
+   {
+     connect_to_db($link);										#  Connect to MySQL
+
+																#  Retrieve salt for uname
+     $query = 'SELECT salt FROM users WHERE uname = "'.$uname.'";';
+     $result = mysql_query($query, $link);
+     if($result == false)
+       {
+         mysql_close($link);
+         croak('dbread');
+       }
+     $num = mysql_num_rows($result);
+     if($num == 0)
+       die('error|Incorrect user name or password');			#  Uname not found
+     $ret = mysql_fetch_array($result, MYSQL_ASSOC);
+     $salt = $ret['salt'];
+																#  Compare hashed pword
+     $query  = 'SELECT kp FROM users';
+     $query .= ' WHERE uname = "'.$uname.'" AND pword = "'.(hash('sha256', $pword)).$salt.'";';
+     $result = mysql_query($query, $link);
+     if($result == false)
+       {
+         mysql_close($link);
+         croak('dbread');
+       }
+     $num = mysql_num_rows($result);
+     if($num == 0)
+       die('error|Incorrect user name or password');			#  Uname not found
+     $ret = mysql_fetch_array($result, MYSQL_ASSOC);
+     $kp = $ret['kp'];
+																#  Log us out of the DB
+     $query = 'UPDATE users SET logged_in = FALSE WHERE kp = '.$kp.';';
+     $result = mysql_query($query, $link);
+     if($result == false)
+       {
+         mysql_close($link);
+         croak('dbupdate');
+       }
+
+     unset($_SESSION['uname']);									#  Destroy session copy of uname
+     unset($_SESSION['pword']);									#  Destroy session copy of pword
+     unset($_SESSION['logged_in']);								#  Destroy session flag
+
+     mysql_close($link);										#  Close the link to DB
+   }
+ else
+   croak('bad_input');
+
+ header('Location: ../../index.php');		#########  NEED TO FORCE REFRESH, TOO !!?!?!?!?!
+ die();
+
+ ?>
